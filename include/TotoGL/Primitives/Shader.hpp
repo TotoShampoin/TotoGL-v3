3e6e0bdf00b45d921051abfb360a3df53d0a910d
TotoShampoin <thomas.garnier@totoshampoin.fr>
2024-02-02 23:19:26 +0100

    single responsibility

--- include/TotoGL/Primitives/Shader.hpp
+++ include/TotoGL/Primitives/Shader.hpp
@@ -4,6 +4,7 @@
 #include <array>
 #include <fstream>
 #include <iostream>
+#include <string>
 #include <variant>
 
 namespace TotoGL {
@@ -25,32 +26,47 @@ public:
     }
     Shader(std::ifstream&& file)
         : Shader() {
+        loadAndCompile(std::move(file));
+    }
+
+    bool loadAndCompile(std::ifstream&& file) {
         load(std::move(file));
+        return compile();
     }
 
     void load(std::ifstream&& file) {
-        auto str = std::string(
+        load(std::string(
             std::istreambuf_iterator<char>(file),
-            std::istreambuf_iterator<char>());
-        auto cstr = str.c_str();
+            std::istreambuf_iterator<char>()));
+    }
 
+    void load(const std::string& src) {
+        auto cstr = src.c_str();
         std::visit([&](auto&& shaderId) {
             glShaderSource(shaderId.id(), 1, &cstr, NULL);
+        },
+            _shader);
+    }
+
+    bool compile() {
+        int success;
+        std::visit([&](auto&& shaderId) {
             glCompileShader(shaderId.id());
-            int success;
             glGetShaderiv(shaderId.id(), GL_COMPILE_STATUS, &success);
             if (!success) {
-                std::array<char, 512> error;
-                glGetShaderInfoLog(shaderId.id(), 512, NULL, error.data());
+                std::array<char, 1024> error;
+                glGetShaderInfoLog(shaderId.id(), 1024, NULL, error.data());
                 std::cerr << error.data();
             }
         },
             _shader);
+        return (bool)success;
     }
 
 private:
     ShaderId _shader;
     friend class ShaderProgram;
+    friend class Material;
 };
 
 } // namespace TotoGL
