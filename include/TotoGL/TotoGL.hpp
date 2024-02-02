11306c0be947c3085dc21f23902a1418d292f1ae
TotoShampoin <thomas.garnier@totoshampoin.fr>
2024-02-02 23:20:48 +0100

    using angle includes

--- include/TotoGL/TotoGL.hpp
+++ include/TotoGL/TotoGL.hpp
@@ -1,16 +1,19 @@
 #pragma once
 
-#include "GPUPointer/Buffers.hpp"
-#include "GPUPointer/Shader.hpp"
-#include "GPUPointer/Texture.hpp"
+#include <TotoGL/Misc/EventListener.hpp>
+#include <TotoGL/Misc/Factory.hpp>
 
-#include "Primitives/Shader.hpp"
-#include "Primitives/ShaderProgram.hpp"
-#include "Primitives/Transformation.hpp"
-#include "Primitives/Vertex.hpp"
-#include "RenderObject/Material.hpp"
-#include "RenderObject/RenderObject.hpp"
-#include "RenderObject/VertexObject.hpp"
+#include <TotoGL/GPUPointer/Buffers.hpp>
+#include <TotoGL/GPUPointer/Shader.hpp>
+#include <TotoGL/GPUPointer/Texture.hpp>
 
-#include "Renderer.hpp"
-#include "Window.hpp"
+#include <TotoGL/Primitives/Shader.hpp>
+#include <TotoGL/Primitives/ShaderProgram.hpp>
+#include <TotoGL/Primitives/Transformation.hpp>
+#include <TotoGL/Primitives/Vertex.hpp>
+#include <TotoGL/RenderObject/Material.hpp>
+#include <TotoGL/RenderObject/Mesh.hpp>
+#include <TotoGL/RenderObject/RenderObject.hpp>
+
+#include <TotoGL/Renderer.hpp>
+#include <TotoGL/Window.hpp>
