a9fd0c8911583b75b4658da8c17a69afcbcc4966
TotoShampoin <thomas.garnier@totoshampoin.fr>
2024-02-07 14:19:42 +0100

    Refactored some stuff out of the main in testing

---
+++ source/aside.cpp
@@ -0,0 +1,77 @@
+#define TOTOGL_IMPLEMENTATIONS
+
+#include "TotoGL/CameraControl/OrbitControl.hpp"
+#include "TotoGL/Primitives/Texture.hpp"
+#include "TotoGL/RenderObject/Camera.hpp"
+#include "TotoGL/RenderObject/RenderObject.hpp"
+#include "TotoGL/RenderObject/ShaderMaterial.hpp"
+#include "TotoGL/Window.hpp"
+// #include <TotoGL/TotoGL.hpp>
+
+void event(
+    TotoGL::Window& window,
+    TotoGL::Camera& camera,
+    bool& holding,
+    TotoGL::OrbitControl& orbit,
+    glm::vec3& velocity) {
+
+    using TotoGL::InputEventName::KEY;
+    using TotoGL::InputEventName::MOUSE_BUTTON;
+    using TotoGL::VectorEventName::CURSOR_POSITION;
+    using TotoGL::VectorEventName::FRAMEBUFFER_SIZE;
+    using TotoGL::VectorEventName::SCROLL;
+
+    // constexpr int WIDTH = 640;
+    constexpr int HEIGHT = 480;
+    constexpr float FOV = glm::radians(70.f);
+
+    window.on(FRAMEBUFFER_SIZE, [&](const TotoGL::VectorEvent& event) {
+        glViewport(0, 0, int(event.x), int(event.y));
+        camera.setPersective(FOV, (float)event.x / event.y, 1.f, 100.f);
+    });
+    window.on(MOUSE_BUTTON, [&](const TotoGL::InputEvent& event) {
+        if (event.button != GLFW_MOUSE_BUTTON_1)
+            return;
+        holding = event.action;
+    });
+    window.on(CURSOR_POSITION, [&](const TotoGL::VectorEvent& event) {
+        if (!holding)
+            return;
+        orbit.rotate(
+            -event.dy / HEIGHT * 2 * FOV,
+            -event.dx / HEIGHT * 2 * FOV);
+    });
+    window.on(SCROLL, [&](const TotoGL::VectorEvent& event) {
+        orbit.distance() += event.y;
+    });
+    window.on(KEY, [&](const TotoGL::InputEvent& event) {
+        if (event.button == GLFW_KEY_UP)
+            velocity.z = -(event.action != GLFW_RELEASE);
+        else if (event.button == GLFW_KEY_DOWN)
+            velocity.z = (event.action != GLFW_RELEASE);
+        if (event.button == GLFW_KEY_LEFT)
+            velocity.x = -(event.action != GLFW_RELEASE);
+        if (event.button == GLFW_KEY_RIGHT)
+            velocity.x = (event.action != GLFW_RELEASE);
+    });
+}
+
+TotoGL::ShaderMaterialFactory::ObjectInstanceId getMaterial() {
+    static const auto mat_id = TotoGL::ShaderMaterialFactory::create(TotoGL::ShaderMaterial(std::ifstream("assets/shader/shader.vert"), std::ifstream("assets/shader/shader.frag")));
+    return mat_id;
+}
+
+TotoGL::RenderObject& makeObject() {
+    static const auto tex_id = TotoGL::TextureFactory::create(TotoGL::Texture(std::ifstream("assets/textures/XYZ.png")));
+    static const auto mesh_id = TotoGL::MeshFactory::create(TotoGL::Mesh::cube());
+    static const auto mat_id = getMaterial();
+
+    static auto& texture = TotoGL::TextureFactory::get(tex_id);
+    // static auto& mesh = TotoGL::MeshFactory::get(mesh_id);
+    static auto& material = TotoGL::ShaderMaterialFactory::get(mat_id);
+
+    material.uniform("u_texture", texture);
+
+    const auto obj_id = TotoGL::RenderObjectFactory::create(TotoGL::RenderObject(mesh_id, mat_id));
+    return TotoGL::RenderObjectFactory::get(obj_id);
+}
--- source/main.cpp
+++ source/main.cpp
@@ -1,8 +1,25 @@
-#include <TotoGL/TotoGL.hpp>
+// #include <TotoGL/TotoGL.hpp>
 
+#include "TotoGL/CameraControl/OrbitControl.hpp"
+#include "TotoGL/Primitives/Clock.hpp"
+#include "TotoGL/RenderObject/Camera.hpp"
+#include "TotoGL/RenderObject/RenderObject.hpp"
+#include "TotoGL/RenderObject/ShaderMaterial.hpp"
+#include "TotoGL/Renderer.hpp"
+#include "TotoGL/Window.hpp"
 #include <glm/glm.hpp>
 
-int main(int argc, const char* argv[]) {
+void event(
+    TotoGL::Window& window,
+    TotoGL::Camera& camera,
+    bool& holding,
+    TotoGL::OrbitControl& orbit,
+    glm::vec3& velocity);
+
+TotoGL::ShaderMaterialFactory::ObjectInstanceId getMaterial();
+TotoGL::RenderObject& makeObject();
+
+int main(int /* argc */, const char** /* argv */) {
     using TotoGL::ShaderType::FRAGMENT;
     using TotoGL::ShaderType::VERTEX;
 
@@ -12,104 +29,69 @@ int main(int argc, const char* argv[]) {
     using TotoGL::VectorEventName::FRAMEBUFFER_SIZE;
     using TotoGL::VectorEventName::SCROLL;
 
-    int width = 640;
-    int height = 480;
-    float fov = glm::radians(70.f);
+    constexpr int WIDTH = 640;
+    constexpr int HEIGHT = 480;
+    constexpr float FOV = glm::radians(70.f);
 
-    auto window = TotoGL::Window(width, height, "a title");
+    auto window = TotoGL::Window(WIDTH, HEIGHT, "a title");
     auto renderer = TotoGL::Renderer();
 
-    const auto tex_id = TotoGL::TextureFactory::create(TotoGL::Texture(std::ifstream("assets/textures/XYZ.png")));
-    const auto mat_id = TotoGL::ShaderMaterialFactory::create(TotoGL::ShaderMaterial(std::ifstream("assets/shader/shader.vert"), std::ifstream("assets/shader/shader.frag")));
-    const auto mesh_id = TotoGL::MeshFactory::create(TotoGL::Mesh::cube());
-    const auto obj_id = TotoGL::RenderObjectFactory::create(TotoGL::RenderObject(mesh_id, mat_id));
+    auto& material = TotoGL::ShaderMaterialFactory::get(getMaterial());
 
-    auto& texture = TotoGL::TextureFactory::get(tex_id);
-    auto& mesh = TotoGL::MeshFactory::get(mesh_id);
-    auto& material = TotoGL::ShaderMaterialFactory::get(mat_id);
-    auto& object = TotoGL::RenderObjectFactory::get(obj_id);
+    auto objects = std::vector<std::reference_wrapper<TotoGL::RenderObject>>();
 
-    auto camera = TotoGL::Camera::Perspective(fov, (float)width / height, .1f, 100.f);
+    for (int i = 0; i < 6; i++) {
+        objects.push_back(makeObject());
+    }
+
+    objects[0].get().translate({ 0, 0, 2 });
+    objects[1].get().translate({ 0, 0, -2 });
+    objects[2].get().translate({ 0, 2, 0 });
+    objects[3].get().translate({ 0, -2, 0 });
+    objects[4].get().translate({ 2, 0, 0 });
+    objects[5].get().translate({ -2, 0, 0 });
+
+    auto camera = TotoGL::Camera::Perspective(FOV, (float)WIDTH / HEIGHT, .1f, 100.f);
     auto clock = TotoGL::Clock();
 
-    auto orbit = TotoGL::OrbitControl(0, 0, 4);
-    // auto freefly = TotoGL::FreeflyControl(0, 0);
+    auto orbit = TotoGL::OrbitControl(0, 0, 8);
 
     bool holding = false;
     glm::vec3 velocity = { 0, 0, 0 };
 
-    object.translate({ 0, 0, 2 });
-
-    // freefly.position() = { 0, 1, 0 };
-
-    window.on(FRAMEBUFFER_SIZE, [&](const TotoGL::VectorEvent& event) {
-        width = event.x;
-        height = event.y;
-        glViewport(0, 0, int(event.x), int(event.y));
-        camera.setPersective(fov, (float)event.x / event.y, 1.f, 100.f);
-    });
-    window.on(MOUSE_BUTTON, [&](const TotoGL::InputEvent& event) {
-        if (event.button != GLFW_MOUSE_BUTTON_1)
-            return;
-        holding = event.action;
-    });
-    window.on(CURSOR_POSITION, [&](const TotoGL::VectorEvent& event) {
-        if (!holding)
-            return;
-        orbit.rotate(
-            -event.dy / height * 2 * fov,
-            -event.dx / height * 2 * fov);
-        // freefly.rotate(
-        //     -event.dx / height * fov,
-        //     -event.dy / height * fov);
-    });
-    window.on(SCROLL, [&](const TotoGL::VectorEvent& event) {
-        orbit.distance() += event.y;
-    });
-    window.on(KEY, [&](const TotoGL::InputEvent& event) {
-        if (event.button == GLFW_KEY_UP)
-            velocity.z = -(event.action != GLFW_RELEASE);
-        else if (event.button == GLFW_KEY_DOWN)
-            velocity.z = (event.action != GLFW_RELEASE);
-        if (event.button == GLFW_KEY_LEFT)
-            velocity.x = -(event.action != GLFW_RELEASE);
-        if (event.button == GLFW_KEY_RIGHT)
-            velocity.x = (event.action != GLFW_RELEASE);
-    });
+    event(window, camera, holding, orbit, velocity);
 
     glEnable(GL_CULL_FACE);
     glCullFace(GL_FRONT);
 
+    glEnable(GL_DEPTH_TEST);
     glEnable(GL_BLEND);
     glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
 
     while (!window.shouldClose()) {
         float time = clock.getTime();
         float delta = clock.getDeltaTime();
-
-        // object.rotate(delta, { 1, 1, 0 });
-
-        auto [width, height] = window.size();
-
-        orbit.apply(camera);
-        orbit.position() += velocity * delta;
-        // freefly.apply(camera);
-        // freefly.moveForward(velocity.z * delta);
-
-        auto modelview = camera.view() * object.transformMatrix();
-        auto normal = glm::mat3(glm::transpose(glm::inverse(modelview)));
+        // auto [width, height] = window.size();
 
         material.uniform("u_time", time);
-        material.uniform("u_texture", texture);
         material.uniform("u_projection", camera.projection());
-        material.uniform("u_modelview", modelview);
-        material.uniform("u_normal", normal);
+
+        orbit.apply(camera);
 
         window.draw([&]() {
             glClearColor(.25, 0., .25, 1.);
-            glClear(GL_COLOR_BUFFER_BIT);
+            glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 
-            object.draw();
+            for (int i = 0; i < 6; i++) {
+                auto& object = objects[i].get();
+                auto modelview = camera.view() * object.transformMatrix();
+                auto normal = glm::mat3(glm::transpose(glm::inverse(modelview)));
+                object.rotation() += glm::vec3(1, 1, 1) * delta;
+
+                material.uniform("u_modelview", modelview);
+                material.uniform("u_normal", normal);
+                objects[i].get().draw();
+            }
         });
     }
 
