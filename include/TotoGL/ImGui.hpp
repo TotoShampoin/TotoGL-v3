8f85af8c8ac0dbb781aa6d77a0b987f8c3a2b6db
TotoShampoin <thomas.garnier@totoshampoin.fr>
2024-02-08 18:13:31 +0100

    it's just the beginning...

---
+++ include/TotoGL/ImGui.hpp
@@ -0,0 +1,32 @@
+#pragma once
+
+#include <imgui.h>
+
+#include <backends/imgui_impl_glfw.h>
+#include <backends/imgui_impl_opengl3.h>
+
+#include "Window.hpp"
+
+namespace TotoGL {
+
+class ImGUI {
+public:
+    ImGUI(Window& window) {
+        IMGUI_CHECKVERSION();
+        ImGui::CreateContext();
+        ImGuiIO& io = ImGui::GetIO();
+        (void)io;
+
+        ImGui_ImplGlfw_InitForOpenGL(window.glfwWindow(), true);
+        ImGui_ImplOpenGL3_Init("#version 460");
+
+        auto& style = ImGui::GetStyle();
+        style.FrameRounding = 2.;
+        style.WindowRounding = 4.;
+    }
+    ~ImGUI() = default;
+
+private:
+};
+
+} // namespace TotoGL
