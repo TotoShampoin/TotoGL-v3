05e9dd40347d00c62b68d36c343ad71214d307f2
TotoShampoin <thomas.garnier@totoshampoin.fr>
2024-02-08 09:47:41 +0100

    Add event binding for mouse rotation

--- include/TotoGL/CameraControl/OrbitControl.hpp
+++ include/TotoGL/CameraControl/OrbitControl.hpp
@@ -1,6 +1,9 @@
 #pragma once
 
+#include <TotoGL/GL.hpp>
+
 #include "TotoGL/RenderObject/Camera.hpp"
+#include "TotoGL/Window.hpp"
 
 namespace TotoGL {
 
@@ -19,6 +22,7 @@ public:
                 glm::cos(_alpha) * glm::sin(_beta) * _distance);
         camera.lookAt(_position, { 0, 1, 0 });
     }
+
     void rotate(float x, float y) {
         static constexpr auto HALF_PI = glm::half_pi<float>();
         static constexpr auto EPSILON = glm::epsilon<float>();
@@ -29,6 +33,24 @@ public:
         if (_alpha < -HALF_PI + EPSILON)
             _alpha = -HALF_PI + EPSILON;
         _beta = glm::mod(_beta + glm::pi<float>(), glm::tau<float>()) - glm::pi<float>();
+    }
+
+    void bindEvents(Window& window) {
+        using InputEventName::MOUSE_BUTTON;
+        using VectorEventName::CURSOR_POSITION;
+
+        static bool is_holding;
+
+        window.on(MOUSE_BUTTON, [&](const InputEvent& event) {
+            if (event.button == GLFW_MOUSE_BUTTON_1) {
+                is_holding = bool(event.action);
+            }
+        });
+        window.on(CURSOR_POSITION, [&](const VectorEvent& event) {
+            if (!is_holding)
+                return;
+            rotate(event.dx, event.dy);
+        });
     }
 
     glm::vec3& position() { return _position; }
