f35dac3ca5720b3179181de5e49af0683935f400
TotoShampoin <thomas.garnier@totoshampoin.fr>
2024-03-07 12:08:26 +0100

    That is a big refactor, if you ask me

--- source/main.cpp
+++ source/main.cpp
@@ -1,3 +1,4 @@
+#include "TotoGL/RenderObject/RenderObject.hpp"
 #include <TotoGL/TotoGL.hpp>
 
 #include <TotoGL/RenderObject/Light.hpp>
@@ -12,8 +13,8 @@ void event(
     glm::vec3& velocity);
 
 TotoGL::ShaderMaterialFactory::ObjectInstanceId getMaterial();
-TotoGL::RenderObject& makeObject();
-TotoGL::RenderObject& makeHelper();
+TotoGL::RenderObjectInstanceId makeObject();
+TotoGL::RenderObjectInstanceId makeHelper();
 
 int main(int /* argc */, const char** /* argv */) {
     using TotoGL::ShaderType::FRAGMENT;
@@ -32,11 +33,13 @@ int main(int /* argc */, const char** /* argv */) {
     auto window = TotoGL::Window(WIDTH, HEIGHT, "a title");
     auto renderer = TotoGL::Renderer();
 
-    auto kirbies = std::vector<std::reference_wrapper<TotoGL::RenderObject>>();
+    auto scene = TotoGL::Scene();
 
+    auto kirbies = std::vector<std::reference_wrapper<TotoGL::RenderObject>>();
     for (int i = 0; i < 6; i++) {
-        auto& kirby = makeObject();
-        kirbies.push_back(kirby);
+        auto kirby = makeObject();
+        scene.add(kirby);
+        kirbies.push_back(TotoGL::RenderObjectFactory::get(kirby));
     }
 
     kirbies[0].get().translate({ 0, 0, 2 });
@@ -46,17 +49,20 @@ int main(int /* argc */, const char** /* argv */) {
     kirbies[4].get().translate({ 2, 0, 0 });
     kirbies[5].get().translate({ -2, 0, 0 });
 
-    auto lights = std::vector<TotoGL::Light>();
+    // auto lights = std::vector<TotoGL::Light>();
     {
-        auto ambient = TotoGL::Light(glm::vec3(1, 1, 1), .25, TotoGL::LightType::AMBIENT);
-        auto dirlight = TotoGL::Light(glm::vec3(1, 1, 1), 1, TotoGL::LightType::DIRECTIONAL);
+        auto ambient_id = TotoGL::LightFactory::create(
+            TotoGL::Light(glm::vec3(1, 1, 1), .25, TotoGL::LightType::AMBIENT));
+        auto dirlight_id = TotoGL::LightFactory::create(
+            TotoGL::Light(glm::vec3(1, 1, 1), 1, TotoGL::LightType::DIRECTIONAL));
+
+        auto& dirlight = TotoGL::LightFactory::get(dirlight_id);
         dirlight.position() = { 3, 3, 3 };
         dirlight.transformation().lookAt({ 0, 0, 0 });
 
-        lights.push_back(ambient);
-        lights.push_back(dirlight);
+        scene.add(ambient_id);
+        scene.add(dirlight_id);
     }
-    auto light_helper = makeHelper();
 
     auto& material = kirbies[0].get().material();
 
@@ -76,41 +82,22 @@ int main(int /* argc */, const char** /* argv */) {
     glEnable(GL_BLEND);
     glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
 
-    // material.uniform("u_light.color", ambient.color());
-    // material.uniform("u_light.strength", ambient.strength());
-    // material.uniform("u_light[0].color", light.color());
-    // material.uniform("u_light[0].strength", light.strength());
-    // material.uniform("u_light[0].type", static_cast<int>(light.type()));
-
     renderer.clearColor({ 0, 0, 0, 1 });
     while (!window.shouldClose()) {
         float time = clock.getTime();
         float delta = clock.getDeltaTime();
-        // auto [width, height] = window.size();
+
+        for (auto& kirby : kirbies) {
+            kirby.get().rotation() += glm::vec3(2, 3, 5) * delta;
+        }
 
         orbit.apply(camera);
 
-        // light.rotate(delta, { 0, 1, 0 });
-        // light_helper.rotate(delta, { 0, 1, 0 });
-
         material.uniform("u_time", time);
-
-        // material.uniform("u_light[0].pos_or_dir",
-        //     glm::mat3(glm::transpose(glm::inverse(camera.view()))) * light.direction());
-        // material.uniform("u_light.pos_or_dir",
-        //     glm::vec3(camera.view() * glm::vec4(point_light.position(), 1)));
 
         window.draw([&]() {
             renderer.clear();
-            for (int i = 0; i < 6; i++) {
-                auto& object = kirbies[i].get();
-                object.rotation() += glm::vec3(2, 3, 5) * delta;
-                renderer.render(object, camera, lights);
-            }
-            light_helper.position() = lights[1].position();
-            light_helper.rotation() = lights[1].transformation().rotation();
-
-            renderer.render(light_helper, camera);
+            renderer.render(scene, camera);
         });
     }
 
