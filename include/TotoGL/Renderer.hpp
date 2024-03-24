753fed53180ab18945df1c67981daa084b49c961
TotoShampoin <thomas.garnier@totoshampoin.fr>
2024-03-24 09:31:34 +0100

    flexibility++ for now

--- include/TotoGL/Renderer.hpp
+++ include/TotoGL/Renderer.hpp
@@ -21,9 +21,6 @@ public:
     void render(TotoGL::Scene& scene, TotoGL::Camera& camera);
     void renderOverrideMaterial(TotoGL::Scene& scene, TotoGL::Camera& camera, TotoGL::ShaderMaterial& material);
 
-private:
-    void init();
-
     void render(TotoGL::RenderObject& object, TotoGL::Camera& camera);
     void render(TotoGL::RenderObject& object, TotoGL::Camera& camera, TotoGL::Light& light, size_t index = 0);
     void render(TotoGL::RenderObject& object, TotoGL::Camera& camera, std::vector<std::reference_wrapper<TotoGL::Light>>& lights);
@@ -31,6 +28,9 @@ private:
     void renderOverrideMaterial(TotoGL::RenderObject& object, TotoGL::Camera& camera, TotoGL::ShaderMaterial& material);
     void renderOverrideMaterial(TotoGL::RenderObject& object, TotoGL::Camera& camera, TotoGL::Light& light, TotoGL::ShaderMaterial& material, size_t index = 0);
     void renderOverrideMaterial(TotoGL::RenderObject& object, TotoGL::Camera& camera, std::vector<std::reference_wrapper<TotoGL::Light>>& lights, TotoGL::ShaderMaterial& material);
+
+private:
+    void init();
 };
 
 } // namespace TotoGL
