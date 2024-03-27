f55b8c834551048176819979301077838129db60
TotoShampoin <thomas.garnier@totoshampoin.fr>
2024-03-27 17:46:12 +0100

    Added MaterialObject, an Object that uses MaterialData and can contain multiple meshes

---
+++ include/TotoGL/RenderObject/MaterialObject.hpp
@@ -0,0 +1,50 @@
+#pragma once
+
+#include "TotoGL/Misc/Factory.hpp"
+#include "TotoGL/Primitives/MaterialData.hpp"
+#include "TotoGL/Primitives/Transform.hpp"
+#include "TotoGL/RenderObject/Mesh.hpp"
+#include "TotoGL/RenderObject/RenderObject.hpp"
+#include "TotoGL/RenderObject/ShaderMaterial.hpp"
+#include <vector>
+
+namespace TotoGL {
+
+class MaterialObject {
+public:
+    MaterialObject(
+        std::vector<MeshInstanceId>&& meshes,
+        std::vector<MaterialData>&& materials,
+        std::vector<uint>&& material_indices);
+
+    RenderObject get(const size_t& index) const;
+    size_t size() const { return _meshes.size(); }
+
+    const std::vector<MeshInstanceId>& meshes() const { return _meshes; }
+    const std::vector<MaterialData>& materials() const { return _materials; }
+    const std::vector<uint>& materialIndices() const { return _material_indices; }
+    ShaderMaterialInstanceId shaderMaterial() const { return _shader_material; }
+
+    MaterialObject& translate(const glm::vec3& translation);
+    MaterialObject& scale(const glm::vec3& factor);
+    MaterialObject& rotate(const float& angle, const glm::vec3& axis);
+    void lookAt(const glm::vec3& target, const glm::vec3& up = { 0, 1, 0 });
+
+    Transform& transformation() { return _transform; }
+    glm::mat4 transformMatrix() const { return _transform.matrix(); }
+    glm::vec3& position() { return _transform.translation(); }
+    glm::vec3& rotation() { return _transform.rotation(); }
+    glm::vec3& scaling() { return _transform.scaling(); }
+
+private:
+    std::vector<MeshInstanceId> _meshes;
+    std::vector<MaterialData> _materials;
+    std::vector<uint> _material_indices;
+    ShaderMaterialInstanceId _shader_material;
+    Transform _transform;
+};
+
+using MaterialObjectFactory = Factory<MaterialObject>;
+using MaterialObjectInstanceId = ObjectInstanceId<MaterialObject>;
+
+} // namespace TotoGL
---
+++ source/RenderObject/MaterialObject.cpp
@@ -0,0 +1,64 @@
+#include "TotoGL/RenderObject/MaterialObject.hpp"
+#include "TotoGL/Primitives/Shader.hpp"
+#include "_embeds/shaders.hpp"
+
+namespace TotoGL {
+
+MaterialObject::MaterialObject(
+    std::vector<MeshInstanceId>&& meshes,
+    std::vector<MaterialData>&& materials,
+    std::vector<uint>&& material_indices)
+    : _meshes(std::move(meshes))
+    , _materials(std::move(materials))
+    , _material_indices(std::move(material_indices)) {
+    static auto PHONG_SHADER = ShaderMaterialFactory::create(ShaderMaterial(
+        VertexShader(vertex_shader),
+        FragmentShader(prong_shader)));
+
+    _shader_material = PHONG_SHADER;
+}
+
+RenderObject MaterialObject::get(const size_t& index) const {
+    auto object = RenderObject(_meshes[index], _shader_material);
+    auto& material = _materials[_material_indices[index]];
+    _shader_material->uniform("u_ambient", material.ambient);
+    _shader_material->uniform("u_diffuse", material.diffuse);
+    _shader_material->uniform("u_specular", material.specular);
+    _shader_material->uniform("u_emissive", material.emissive);
+    _shader_material->uniform("u_shininess", material.shininess);
+    if (material.ambient_texture) {
+        _shader_material->uniform("u_ambient_texture", material.ambient_texture.value());
+    }
+    if (material.diffuse_texture) {
+        _shader_material->uniform("u_diffuse_texture", material.diffuse_texture.value());
+    }
+    if (material.specular_texture) {
+        _shader_material->uniform("u_specular_texture", material.specular_texture.value());
+    }
+    if (material.emissive_texture) {
+        _shader_material->uniform("u_emissive_texture", material.emissive_texture.value());
+    }
+    object.transformation() = _transform;
+    return object;
+}
+
+MaterialObject& MaterialObject::translate(const glm::vec3& translation) {
+    _transform.translate(translation);
+    return *this;
+}
+
+MaterialObject& MaterialObject::scale(const glm::vec3& factor) {
+    _transform.scale(factor);
+    return *this;
+}
+
+MaterialObject& MaterialObject::rotate(const float& angle, const glm::vec3& axis) {
+    _transform.rotate(angle, axis);
+    return *this;
+}
+
+void MaterialObject::lookAt(const glm::vec3& target, const glm::vec3& up) {
+    _transform.lookAt(target, up);
+}
+
+}; // namespace TotoGL
